9467230b1951edfedf21679d36220b62d2c94a9b
shiyan_xiaobin <shiyan_xiaobin@163.com>
2014-09-03 08:19:08 +0800

    ss

---
+++ data/config/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+ if(!defined('HDPHP_PATH')) exit;
+return array (
+  'webname' => '极客万博',
+  'webstat' => 1,
+  'email' => 'geekwebcn@gmail.com',
+  'qq' => '929272607',
+  'weibo' => '真-的爱-你',
+  'WATER_ON' => 0,
+  'tel' => '18766031424',
+  'service_tel' => '18766031424',
+  'address' => '山东省 德州市 大学西路 566号',
+  'tpl_type' => 'default',
+);?>
---
+++ data/config/db.inc.php
@@ -0,0 +1,11 @@
+<?php if(!defined('HDPHP_PATH')) exit;
+ return array (
+  'DB_DRIVER' => 'mysqli',
+  'DB_CHARSET' => 'utf8',
+  'DB_HOST' => '127.0.0.1',
+  'DB_PORT' => 3306,
+  'DB_USER' => 'root',
+  'DB_PASSWORD' => 'admin',
+  'DB_DATABASE' => 'db_cms',
+  'DB_PREFIX' => '',
+);?>
